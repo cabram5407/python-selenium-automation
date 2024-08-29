fb7b57b1f55411ef986be4dcf688abb3d3d96f58
Charmaine Abram <cabram1967@comcast.net>
2024-08-28 18:07:37 -0700

    Updated project with new file.

---
+++ Target_Search.py
@@ -0,0 +1,8 @@
+from IPython.utils.coloransi import value
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
