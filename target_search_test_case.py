99ce66ca95dc44507509a22feba5741ebda62d28
c_abram5407 <cabram1967@comcast.net>
2024-09-06 17:58:20 -0700

    css selectors added.

--- target_search_test_case.py
+++ target_search_test_case.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 import numpy
+
 #installs chrome driver
 driver_path = ChromeDriverManager().install()
 # create a new Chrome browser instance
@@ -13,12 +14,16 @@ service = Service(driver_path)
 driver: WebDriver = webdriver.Chrome(service=service)
 driver.maximize_window()
 
+#Target test case
 driver.get('https://www.target.com/')
+
 #Search for text, "Tea"
 driver.find_element(By.ID, 'search').send_keys('tea')
+
 #click on tea
 driver.find_element(By.XPATH,"//button[@data-test='@web/Search/SearchButton']").click()
 sleep(10)
+
 #Verification for result
 actual_result = driver.find_element(By.XPATH, "//div[@data-test='resultsHeading']").text
 expected_result = 'tea'
