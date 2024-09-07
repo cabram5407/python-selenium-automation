965cee1f25504d33f1352715b1527362670f90d9
c_abram5407 <cabram1967@comcast.net>
2024-09-06 20:56:36 -0700

    Homework 3 for locators at Target test case.

--- homework_3_css_locators.py
+++ homework_3_css_locators.py
@@ -15,9 +15,44 @@ driver.maximize_window()
 # open the url
 driver.get('https://www.amazon.com/')
 
+#From Amazon home page, click on arrow in top navigation
+driver.find_element(By.XPATH, "//*[text()='Account & Lists' and @class='nav-line-2 ']")
 
-driver.find_element(By.CSS_SELECTOR, "[aria-label='Amazon']")
-driver.find_element(By.CSS_SELECTOR, "[class*='Create account']")
-driver.find_element(By.CSS_SELECTOR, "[id='ap_customer_name']")
-driver.find_element(By.CSS_SELECTOR, "[id='ap_email']")
-driver.find_element(By.CSS_SELECTOR, "[id='ap_password']")
+#Click on Creat Account button
+driver.find_element(By.CSS_SELECTOR, "#createAccountSubmit")
+
+driver.find_element(By.XPATH, "//h1[@class='a-spacing-small')
+driver.find_element(By.CSS_SELECTOR, "#ap_customer_name")
+driver.find_element(By.CSS_SELECTOR, "#ap_email")
+driver.find_element(By.CSS_SELECTOR, "#ap_password")
+driver.find_element(By.XPATH, "//div[@class='a-alert-content']")
+driver.find_element(By.CSS_SELECTOR, "#ap_password_check")
+driver.find_element(By.CSS_SELECTOR, "#continue")
+
+#Condition of use
+driver.find_element(By.XPATH, "//a[contains(@href, 'ap_register_notification_condition_of_use')]")
+
+#Privacy notice
+driver.find_element(By.XPATH, "//a[contains(@href,'ap_register_notification_privacy_notice')]")
+
+#Already have an account sign-in
+driver.find_element(By.XPATH, "//a[@class='a-link-emphasis']")
+
+#Target test case that opens Target.com, clicks on cart icon and verify empty cart message
+driver.get('https://www.target.com/')
+
+#Click on shopping cart icon"
+driver.find_element(By.XPATH, "//div[@data-test='@web/CartIcon')").click
+
+sleep(10)
+
+#Verification that cart is empty.
+actual_result=driver.find_element(By.XPATH, "//h1[text()='Your cart is empty']").text
+
+#verify is true but if false it will fail.
+assert expected_result in actual_result, f'{expected_result}, got actual {actual_result}'
+print('Test case passed')
+driver.quit()
+
+#Target test case to verify a user log out and user can navigate to sign-in.
+
