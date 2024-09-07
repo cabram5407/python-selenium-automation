ff8e3cfb717874894c9cabed0c701b360a7b89dc
c_abram5407 <cabram1967@comcast.net>
2024-09-06 21:28:16 -0700

    Homework 3 for locators at Target test case again.

--- homework_3_css_locators.py
+++ homework_3_css_locators.py
@@ -42,7 +42,7 @@ driver.find_element(By.XPATH, "//a[@class='a-link-emphasis']")
 driver.get('https://www.target.com/')
 
 #Click on shopping cart icon"
-driver.find_element(By.XPATH, "//div[@data-test='@web/CartIcon')").click
+driver.find_element(By.XPATH, "//div[@data-test='@web/CartIcon']").click()
 
 sleep(10)
 
@@ -55,4 +55,17 @@ print('Test case passed')
 driver.quit()
 
 #Target test case to verify a user log out and user can navigate to sign-in.
+driver.get('https://www.target.com/')
 
+driver.find_element(By.XPATH, "//span[text()='Sign in']").click()
+
+#side navigation menu to sign-in
+driver.find_element(By.XPATH, "//a[@data-test='accountNav-signIn']").click()
+
+#verify sign-in form opened
+actual_result = driver.find_element(By.XPATH, "//span[text()='Sign into your Target account']").text
+
+assert expected_result in actual_result, f'{expected_result}, got actual {actual_result}'
+print('Test case passed')
+
+driver.quit()
