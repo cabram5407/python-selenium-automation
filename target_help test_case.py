c7e10f1804122a37095c5f98ec7384f1c112f7b5
c_abram5407 <cabram1967@comcast.net>
2024-09-14 17:24:42 -0700

    Target test cases, steps

--- target_help test_case.py
+++ target_help test_case.py
@@ -18,3 +18,19 @@ driver.implicitly_wait(4)
 driver.get('https://help.target.com/help')
 
 #Verify elements are present
+driver.get.find_element(By.CSS_SELECTOR, "[class='custom-h2']").click()
+
+#search input field
+driver.get.find_element(By.CSS_SELECTOR, "[id='j_id0:j_id2:j_id32:name']".click()
+driver,get.find_element(By.CSS_SELECTOR, "[class='btn-sm search-btn']".click()
+
+#what would you like to do boxes (10)
+driver.get.find.element(By.CSS_SELECTOR, "[class*='grid_6']").click()
+driver.get.find.element(BY.CSS_SELECTOR, "[class*='grid_4 boxSmallr txtAC bigbox2']").click()
+
+#pruduct recalls and contact us
+driver.get.find.element(By.CSS_SELECTOR, "[class*='grid_6']").click()
+
+#browse all help pages
+driver.get.find.element(By.CSS_SELECTOR, "//*[text()='Browse all Help pages']")
+
